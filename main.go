e01cc90c9b2cb03305965144758c405162a61353
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2017-08-04 15:45:13 +0530

    Initial version of a github oauth authenticator

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/pat"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+)
+
+func main() {
+	goth.UseProviders(
+		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
+	)
+
+	p := pat.New()
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+			fmt.Fprintln(res, err)
+			return
+		}
+		t := template.Must(template.ParseFiles("tmpl/user.html"))
+		t.Execute(res, user)
+	})
+
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		// try to get the user without re-authenticating
+		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
+			t := template.Must(template.ParseFiles("tmpl/user.html"))
+			t.Execute(res, gothUser)
+		} else {
+			gothic.BeginAuthHandler(res, req)
+		}
+	})
+
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		t := template.Must(template.ParseFiles("tmpl/index.html"))
+		t.Execute(res, nil)
+	})
+	
+	http.ListenAndServe(":3000", p)
+}
