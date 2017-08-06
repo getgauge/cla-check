283a00e8e7eac0f46e4009bfb692031fe426a35b
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2017-08-06 20:05:45 +0530

    Moved the cla to layout and yield to layout

--- main.go
+++ main.go
@@ -5,28 +5,17 @@ import (
 	"github.com/kataras/iris/context"
 )
 
-type mypage struct {
-	Title   string
-	Message string
-}
-
 func main() {
 	app := iris.New()
 
 	app.StaticWeb("/static", "./resources")
 
-	app.RegisterView(iris.HTML("./templates", ".html").Layout("layout.html"))
-	// TIP: append .Reload(true) to reload the templates on each request.
+	app.RegisterView(iris.HTML("./templates", ".html").Layout("layout.html").Reload(true))
 
 	app.Get("/", func(ctx context.Context) {
 		ctx.Gzip(true)
-		ctx.ViewData("", mypage{"My Page title", "Hello world!"})
-		ctx.View("index.html")
-		// Note that: you can pass "layout" : "otherLayout.html" to bypass the config's Layout property
-		// or view.NoLayout to disable layout on this render action.
-		// third is an optional parameter
+		ctx.View("cla.html")
 	})
 
-	// http://localhost:8080
 	app.Run(iris.Addr(":8080"))
 }
