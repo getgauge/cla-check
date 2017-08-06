3de1adb46f8ba0232891ea3de8a759674c68c9f4
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2017-08-06 09:53:25 +0530

    Create a layout for html

--- main.go
+++ main.go
@@ -13,13 +13,15 @@ type mypage struct {
 func main() {
 	app := iris.New()
 
+	app.StaticWeb("/static", "./resources")
+
 	app.RegisterView(iris.HTML("./templates", ".html").Layout("layout.html"))
 	// TIP: append .Reload(true) to reload the templates on each request.
 
 	app.Get("/", func(ctx context.Context) {
 		ctx.Gzip(true)
 		ctx.ViewData("", mypage{"My Page title", "Hello world!"})
-		ctx.View("mypage.html")
+		ctx.View("index.html")
 		// Note that: you can pass "layout" : "otherLayout.html" to bypass the config's Layout property
 		// or view.NoLayout to disable layout on this render action.
 		// third is an optional parameter
