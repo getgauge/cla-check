241f71e82879ebe39d7cdd67ba832e657df6b017
BugDiver <vinaysh@thoughtworks.com>
2017-09-21 14:36:47 +0530

    added postgres db drivers import.

--- data/contributor.go
+++ data/contributor.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/getgauge/cla-check/configuration"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // User data from Github after signing the CLA
